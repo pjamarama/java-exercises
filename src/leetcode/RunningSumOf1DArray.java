8d6f4fa3562d7970f2c9c4062bb98950d0e1e857
Alexey <lukomorie2001@gmail.com>
2020-11-07 21:44:33 +0300

    Leetcode exercise 'Running Sum of 1D Array'

--- src/leetcode/RunningSumOf1DArray.java
+++ src/leetcode/RunningSumOf1DArray.java
@@ -1,16 +1,16 @@
 /**
  * Given an array nums. We define a running sum of an array as runningSum[i] = sum(nums[0]…nums[i]).
- *
+ * <p>
  * Return the running sum of nums.
- *
- *
+ * <p>
+ * <p>
  * Example 1:
- *
+ * <p>
  * Input: nums = [1,2,3,4]
  * Output: [1,3,6,10]
  * Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
  * Example 2:
- *
+ * <p>
  * Input: nums = [1,1,1,1,1]
  * Output: [1,2,3,4,5]
  * Explanation: Running sum is obtained as follows: [1, 1+1, 1+1+1, 1+1+1+1, 1+1+1+1+1].
@@ -20,25 +20,23 @@ package leetcode;
 
 public class RunningSumOf1DArray {
     public static void main(String[] args) {
-        printArray(runningSum(new int[] {1,1,1,1,1}));
-        printArray(runningSum(new int[] {1,2,3,4}));
+        printArray(runningSum(new int[]{1, 1, 1, 1, 1}));
+        printArray(runningSum(new int[]{1, 2, 3, 4}));
     }
 
     public static int[] runningSum(int[] nums) {
-        int[] result = new int[nums.length];
-        int a = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            a += nums[i];
-            result[i] = a;
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
         }
 
-        return result;
+        return nums;
     }
 
     private static void printArray(int[] nums) {
         for (int i : nums) {
             System.out.print(i + " ");
         }
+        System.out.println();
     }
 }
